984d809a79bba5aa4ffaa3f3e371014d038c4d32
TX-9 <jacobleecs@gmail.com>
2020-02-25 06:38:00 -0500

    add Ingredient Comp

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,8 +1,49 @@
-import React from "react";
+import React, { Component } from "react";
+import classes from './BurgerIngredient.module.css'
+import PropTypes from 'prop-types';
 
-const ingredient = (props) => {
-    let ingredient = null;
-    
+class BurgerIngredient extends Component {
+    render() {
+        let ingredient = null;
+
+        switch (this.props.type) {
+            case ('bread-bottom'):
+                ingredient = <div className={classes.BreadBottom}></div>;
+                break;
+
+            case ('bread-top'):
+                ingredient = (
+                        <div className={classes.BreadTop}>
+                            <div className={classes.Seeds1}></div>
+                            <div className={classes.Seeds2}></div>
+                        </div>
+                    );
+                break;
+
+            case ('meat'):
+                ingredient = <div className={classes.meat}></div>;
+                break;
+
+            case ('cheese'):
+                ingredient = <div className={classes.cheese}></div>;
+                break;
+
+            case ('bacon'):
+                ingredient = <div className={classes.bacon}></div>;
+                break;
+
+            case ('salad'):
+                ingredient = <div className={classes.salad}></div>;
+                break;
+            default:
+                ingredient = null;
+        }
+        return ingredient;
+    }
+}
+
+BurgerIngredient.prototype = {
+        type: PropTypes.string.isRequired
 };
 
-export default ingredient;
+export default BurgerIngredient;
