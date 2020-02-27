9188c660ec366c300e1da270cb3d537111932fc5
TX-9 <jacobleecs@gmail.com>
2020-02-27 06:43:51 -0500

    show Order Summary modal

--- src/containers/BurgerKing/BurgerKing.js
+++ src/containers/BurgerKing/BurgerKing.js
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 import Aux from '../../hoc/Aux_';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
-
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OderSummary/OderSummary'
 const INGREDIENT_PRICES = {
     salad: 0.5,
     cheese: 0.4,
@@ -77,6 +78,9 @@ class BurgerKing extends Component {
         }
         return (
             <Aux>
+                <Modal>
+                    <OrderSummary ingredients={this.state.ingredients}/>
+                </Modal>
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildControls
                     ingredientAdded={this.addIngredientHandler}
