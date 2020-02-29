4685b3fb1630b3578cc81f3f45da7ab0f529ca28
TX-9 <jacobleecs@gmail.com>
2020-02-29 15:50:31 -0500

    global error handler with hoc

--- src/containers/BurgerKing/BurgerKing.js
+++ src/containers/BurgerKing/BurgerKing.js
@@ -6,6 +6,7 @@ import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
 import axios from '../../axios-orders';
 import Spinner from '../../components/UI/Spinner/Spinner';
+import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -99,12 +100,10 @@ class BurgerKing extends Component {
         }
         axios.post('/orders.json', order)
             .then(res => {
-                //this.setState({loading:false,purchasing:false});
-                console.log(res)
+                this.setState({loading:false,purchasing:false});
             })
             .catch(error => {
                 this.setState({loading:false});
-                console.log(error)
             });
     }
     render() {
@@ -141,4 +140,4 @@ class BurgerKing extends Component {
     }
 }
 
-export default BurgerKing;
+export default withErrorHandler(BurgerKing, axios);
